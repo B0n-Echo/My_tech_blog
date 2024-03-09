5bb4c769b6893f411fe2d5dddc45859c1922840c
Nikhilesh Pandey <nikhilesh.pandey@deliverysolutions.co>
2024-03-09 20:06:10 +0530

    feat(nikhil_navigation): initial setup for navigation

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,12 @@
+import Featured from "@/components/featured/Featured";
 import styles from "./homepage.module.css";
+import CategoryList from "@/components/categoryList/CategoryList";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+    </div>
+  )
 }
