252981376946bc12551eb35212b2cd7e2bb81127
Nikhilesh Pandey <nikhilesh.pandey@deliverysolutions.co>
2024-03-21 05:55:55 +0530

    feat(nikhil): updating css/code changes for single page module

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,6 +1,7 @@
 import Menu from "@/components/Menu/Menu";
 import styles from "./singlePage.module.css";
 import Image from "next/image";
+import Comments from "@/components/comments/Comments";
 
 const SinglePage = async () => {
   return (
@@ -9,22 +10,55 @@ const SinglePage = async () => {
         <div className={styles.textContainer}>
           <h1 className={styles.title}>Lorem ipsum adipisicing elit.</h1>
           <div className={styles.user}>
-            XXXX
+            <div className={styles.userImageContainer}>
+              <Image
+                src="/p1.jpeg"
+                alt=""
+                fill
+                className={styles.avatar}
+              />
+            </div>
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>Name</span>
+              <span className={styles.username}>John Doe</span>
               <span className={styles.date}>01.01.2024</span>
             </div>
           </div>
         </div>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
+        <div className={styles.imageContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+        </div>
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <div
-            className={styles.description}
-          />
+          <div className={styles.description}>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+              inventore. Non veniam cupiditate voluptatem laboriosam quae
+              nostrum, provident repudiandae vitae, a ut minus unde
+              reprehenderit ratione aperiam at sequi quod.
+            </p>
+            <h2>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+              inventore. Non veniam cupiditate voluptatem laboriosam quae
+              nostrum, provident repudiandae vitae, a ut minus unde
+              reprehenderit ratione aperiam at sequi quod.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+              inventore. Non veniam cupiditate voluptatem laboriosam quae
+              nostrum, provident repudiandae vitae, a ut minus unde
+              reprehenderit ratione aperiam at sequi quod.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+              inventore. Non veniam cupiditate voluptatem laboriosam quae
+              nostrum, provident repudiandae vitae, a ut minus unde
+              reprehenderit ratione aperiam at sequi quod.
+            </p>
+          </div>
           <div className={styles.comment}>
           </div>
         </div>
