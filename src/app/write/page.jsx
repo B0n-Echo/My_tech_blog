c1aeab3caa48dadc751a133d7bb3c798b5b5b077
Nikhilesh Pandey <nikhilesh.pandey@deliverysolutions.co>
2024-03-26 12:15:41 +0530

    feat(nikhil): Adding ReactQuill to write module

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -3,17 +3,15 @@
 import React, { useState } from 'react'
 import styles from "./writePage.module.css";
 import Image from 'next/image';
+import "react-quill/dist/quill.bubble.css";
+import ReactQuill from "react-quill";
 
 function WritePage() {
   const [open, setOpen] = useState(false);
 
   return (
     <div className={styles.container}>
-      <input
-        type="text"
-        placeholder="Title"
-        className={styles.input}
-      />
+      <input type="text" placeholder="Title" className={styles.input} />
       <select className={styles.select}>
         <option value="style">style</option>
         <option value="fashion">fashion</option>
@@ -23,16 +21,12 @@ function WritePage() {
         <option value="coding">coding</option>
       </select>
       <div className={styles.editor}>
-        <button className={styles.button}>
-        <Image src="/plus.png" alt="" width={16} height={16} />
+        <button className={styles.button} onClick={() => setOpen(!open)}>
+          <Image src="/plus.png" alt="" width={16} height={16} />
         </button>
         {open && (
           <div className={styles.add}>
-            <input
-              type="file"
-              id="image"
-              style={{ display: "none" }}
-            />
+            <input type="file" id="image" style={{ display: "none" }} />
             <button className={styles.addButton}>
               <label htmlFor="image">
                 <Image src="/image.png" alt="" width={16} height={16} />
@@ -46,9 +40,17 @@ function WritePage() {
             </button>
           </div>
         )}
+        <ReactQuill
+          className={styles.textArea}
+          theme="bubble"
+          placeholder="Tell your story..."
+        />
       </div>
-      </div>
-  )
+      <button className={styles.publish}>
+        Publish
+      </button >
+    </div>
+  );
 }
 
 export default WritePage
