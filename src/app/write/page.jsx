7849574d02c0113cb5ca9bbd72ae03c9f4b67ea5
Nikhilesh Pandey <nikhilesh.pandey@deliverysolutions.co>
2024-04-12 08:19:55 +0530

    refactor(nikhil): refactoring code for write page

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -5,9 +5,23 @@ import styles from "./writePage.module.css";
 import Image from 'next/image';
 import "react-quill/dist/quill.bubble.css";
 import ReactQuill from "react-quill";
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 function WritePage() {
+  const { status } = useSession(); 
+  const router = useRouter();
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  if (status === "unauthenticated") {
+    router.push("/write");
+  }
+
 
   return (
     <div className={styles.container}>
@@ -43,6 +57,7 @@ function WritePage() {
         <ReactQuill
           className={styles.textArea}
           theme="bubble"
+          value={value}
           placeholder="Tell your story..."
         />
       </div>
